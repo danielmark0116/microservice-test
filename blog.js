fe5d4dcc66bd8450934265689a38585bcdc49475
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-10-15 17:33:07 +0200

    html pages

--- blog.js
+++ blog.js
@@ -1,13 +1,14 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = 9000;
 
 app.get('/', (req, res) => {
-  res.send('main blog page');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/api', (req, res) => {
-  res.send('api blog page');
+  res.sendFile(path.join(__dirname, 'api.html'));
 });
 
 app.listen(port, () => console.log(`App running on port ${port}`));
